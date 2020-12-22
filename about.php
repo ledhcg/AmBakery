d8ffc1428552d783152fc96c04599fe5eb9fb940
ledhcg <dinhcuong.firewin99@gmail.com>
2020-12-22 12:28:25 +0300

    Fix bug

--- about.php
+++ about.php
@@ -19,13 +19,13 @@
                 <div class="card">
                     <div class="card-content">
                         <img class="card-img-top img-fluid" src="images/templates/about.jpg" alt="" />
-                        
+
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-6">
                                     <div class="divider">
                                         <div class="divider-text"><h3>AmBakery</h3></div>
-                                    </div>                                    
+                                    </div>
                                         <p class="card-text" style="font-size: 16px; text-align: center">
                                         Это программный проект для управления продажами и заказами. Программное обеспечение обеспечивает функции импорта и экспорта продуктов, заказов и услуг для небольших хлебных магазинов.
                                         Управление продуктом: продукт, торговая марка, клиент, продавец, заказ, статистика производства и импорт материалов.
@@ -35,22 +35,22 @@
                                         Студент: <strong>Ле Динь Кыонг</strong>  -  Группа: <strong>ИКБО-07-19</strong>  -  Университет: <strong>МИРЭА</strong>
                                         </p>
                                         <p class="card-text" style="font-size: 16px; text-align: center">
-                                            <a href="https://github.com/ledhcg"  style="color: #727E8C"><i style="font-size: 20px; padding: 5px 5px 5px 5px" class="fab fa-github"></i></a>  <a href="https://vk.com/kifirixix" style="color: #727E8C"><i style="font-size: 20px; padding: 5px 5px 5px 5px" class="fab fa-vk"></i></a>  <a href="https://www.facebook.com/ledhcg" style="color: #727E8C"><i style="font-size: 20px; padding: 5px 5px 5px 5px" class="fab fa-facebook"></i></a>  
+                                            <a href="https://github.com/ledhcg"  style="color: #727E8C"><i style="font-size: 20px; padding: 5px 5px 5px 5px" class="fab fa-github"></i></a>  <a href="https://vk.com/kifirixix" style="color: #727E8C"><i style="font-size: 20px; padding: 5px 5px 5px 5px" class="fab fa-vk"></i></a>  <a href="https://www.facebook.com/ledhcg" style="color: #727E8C"><i style="font-size: 20px; padding: 5px 5px 5px 5px" class="fab fa-facebook"></i></a>
                                         </p>
 
                                         <p class="card-text" style="font-size: 16px; text-align: center" >
-                                            <strong>Website:</strong> <a style="color: #727E8C" href="https://dinhcuong.me/"><i style="font-size: 18px;" >http://dinhcuong.me</i></a>  -  <strong>Email:</strong> <a style="color: #727E8C" href="mailto: dinhcuong.firewin99@gmail.com"><i style="font-size: 18px;" >dinhcuong.firewin99@gmail.com></i></a>
+                                            <strong>Website:</strong> <a style="color: #727E8C" href="https://dinhcuong.me/"><i style="font-size: 18px;" >https://dinhcuong.me</i></a>  -  <strong>Email:</strong> <a style="color: #727E8C" href="mailto: dinhcuong.firewin99@gmail.com"><i style="font-size: 18px;" >dinhcuong.firewin99@gmail.com</i></a>
                                         </p>
 
                                         <div class="d-flex justify-content-center">
                                             <img class="" width="40%" src="images/admin/dinhcuong.png" alt="" />
-                                            
+
                                         </div>
                                     <code><hr></code>
                                 </div>
                                 <div class="col-6 d-flex justify-content-center">
                                     <img class="" width="90%" src="images/logo/AmBakeryTextLogo.svg" alt="" />
-                                      
+
                                 </div>
                             </div>
                         </div>
